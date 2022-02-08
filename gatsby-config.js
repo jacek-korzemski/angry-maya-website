8660dbec0d2fac6f08eafdca4d68b2ca91c82e20
Jacek Korzemski <wrp40.000@wp.pl>
2022-02-08 01:00:02 +0100

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Gatsby template in my style`,
+    description: `Just a cleanup of default Gatsby boilerplate`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `https://localhost:8000/`,
   },
   plugins: [
+    `gatsby-plugin-root-import`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `assets`,
+        path: `${__dirname}/src/assets`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -28,7 +29,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/placeholder-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "assets/main-css/Bootstrap.grid.css";
+import "assets/main-css/Layout.css";
+
+const Layout = ({ children }) => {
+    return <>{children}</>;
+};
+
+export default Layout;
